6047cff7f52bb2b4f4de30a15cc7bb5a05d26ed4
TakutoMiyata <miyatak0110@gmail.com>
2025-09-25 14:15:23 +0900

    バッチサイズを変更，不要なconfigを削除

--- fedsa_ftl_standalone/run_experiments.sh
+++ fedsa_ftl_standalone/run_experiments.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 1つ目のジョブを実行
-nohup python quickstart_resnet_fedavg.py \
+nohup python quickstart_resnetpy \
   --config configs/experiment_configs_iid/IID-FedAvg.yaml \
   --round 100
 
 # 上が終了したら次を実行
 nohup python quickstart_resnet.py \
-  --config configs/experiment_configs_iid/IID-FedSA-LoRA.yaml \
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA.yaml \
   --round 100
