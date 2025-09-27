a57bb0358224fcb8b6d6fda9ba2c845f4ec411a3
TakutoMiyata <miyatak0110@gmail.com>
2025-09-27 22:48:18 +0900

    実行ファイルを修正

--- fedsa_ftl_standalone/run_experiments.sh
+++ fedsa_ftl_standalone/run_experiments.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# 上が終了したら次を実行
-nohup python quickstart_resnet.py \
+python quickstart_resnet.py \
   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon2.yaml \
-  --round 100 &
+  --round 100
 
-# 上が終了したら次を実行
-nohup python quickstart_resnet.py \
+python quickstart_resnet.py \
   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon4.yaml \
-  --round 100 &
+  --round 100
 
-# 上が終了したら次を実行
-nohup python quickstart_resnet.py \
+python quickstart_resnet.py \
   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon8.yaml \
-  --round 100 &
+  --round 100
 
-# 上が終了したら次を実行
-nohup python quickstart_resnet.py \
+python quickstart_resnet.py \
   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon16.yaml \
-  --round 100 &
-
+  --round 100
