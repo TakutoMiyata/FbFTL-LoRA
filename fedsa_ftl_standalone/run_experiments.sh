fa99b40970f0b9ac405975a31ed8ef9965820f5c
TakutoMiyata <miyatak0110@gmail.com>
2025-09-27 22:38:35 +0900

    実験用epsion

--- fedsa_ftl_standalone/run_experiments.sh
+++ fedsa_ftl_standalone/run_experiments.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# 1つ目のジョブを実行
-nohup python quickstart_resnetpy \
-  --config configs/experiment_configs_iid/IID-FedAvg.yaml \
-  --round 100
+# 上が終了したら次を実行
+nohup python quickstart_resnet.py \
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon2.yaml \
+  --round 100 &
 
 # 上が終了したら次を実行
 nohup python quickstart_resnet.py \
-  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA.yaml \
-  --round 100
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon4.yaml \
+  --round 100 &
+
+# 上が終了したら次を実行
+nohup python quickstart_resnet.py \
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon8.yaml \
+  --round 100 &
+
+# 上が終了したら次を実行
+nohup python quickstart_resnet.py \
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon16.yaml \
+  --round 100 &
+
