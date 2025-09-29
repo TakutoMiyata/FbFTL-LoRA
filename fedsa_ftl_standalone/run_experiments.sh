3d1f1c63a9a4ba2af50e0162b419fc830a78e653
TakutoMiyata <miyatak0110@gmail.com>
2025-09-30 02:56:04 +0900

    inputsize160に合わせて設定を修正（試し）

--- fedsa_ftl_standalone/run_experiments.sh
+++ fedsa_ftl_standalone/run_experiments.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# python quickstart_resnet.py \
-#   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon2.yaml \
-#   --round 100
+python quickstart_resnet.py \
+  --config configs/experiment_configs_iid/IID-FedSA-LoRA.yaml \
+  --round 100
+
+python quickstart_resnet_fedavg.py \
+  --config configs/experiment_configs_iid/IID-FedAvg.yaml \
+  --round 100
+
+python quickstart_resnet.py \
+  --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon2.yaml \
+  --round 100
 
 python quickstart_resnet.py \
   --config configs/experiment_configs_iid/IID-DP-FedSA-LoRA-epsilon4.yaml \
@@ -17,9 +25,25 @@ python quickstart_resnet.py \
   --round 100
 
 # python quickstart_resnet.py \
-#   --config configs/experiment_configs_iid/IID-FedSA-LoRA.yaml \
+#   --config configs/experiment_configs_non_iid/non-IID-FedSA-LoRA.yaml \
+#   --round 100
+  
+# python quickstart_resnet_fedavg.py \
+#   --config configs/experiment_configs_non_iid/non-IID-FedAvg.yaml \
 #   --round 100
 
-# python quickstart_resnet_fedavg.py \
-#   --config configs/experiment_configs_iid/IID-FedAvg.yaml \
+# python quickstart_resnet.py \
+#   --config configs/experiment_configs_non_iid/non-IID-DP-FedSA-LoRA-epsilon2.yaml \
+#   --round 100
+
+# python quickstart_resnet.py \
+#   --config configs/experiment_configs_non_iid/non-IID-DP-FedSA-LoRA-epsilon4.yaml \
+#   --round 100
+
+# python quickstart_resnet.py \
+#   --config configs/experiment_configs_non_iid/non-IID-DP-FedSA-LoRA-epsilon8.yaml \
+#   --round 100
+
+# python quickstart_resnet.py \
+#   --config configs/experiment_configs_non_iid/non-IID-DP-FedSA-LoRA-epsilon16.yaml \
 #   --round 100
